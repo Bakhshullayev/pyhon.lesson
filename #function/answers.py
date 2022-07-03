79d180a09317eaf9922be8085c143ad8c006898c
Bakhshullayev <aser6699069@gmail.com>
2022-07-03 14:02:57 +0500

    answers add new function

--- #function/answers.py
+++ #function/answers.py
@@ -24,7 +24,16 @@ def func4(num1, num2):
     return "{son1} = {son2}".format(son2=num2, son1=num1)
 
 
-def func5(x, y):
+def func5(x, y=2):
     """sonlarni x va y ning kvadaratini aniqlovchi funksiya"""
     return f'{x ** y}'
 
+
+def func6(x):
+    """soning 2 dan 10 gacha bo'lgan sonlarga qoldiqsiz bo'linmasi"""
+    for son in range(2, 11):
+        if x % son == 0:
+            print(f'{x}ga {son} qoldiqsiz bo\'linadi')
+
+
+func6(70)
